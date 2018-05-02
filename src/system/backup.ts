b156fc1507b185d41310cea32b8defa8dbd2485d
Seb <seb@seb.fr>
2018-05-02 19:10:15 +0200

    feat(Backup): CRON

--- src/system/backup.ts
+++ src/system/backup.ts
@@ -13,7 +13,7 @@ export class Backup {
         let childProcess = child.exec('crontab -l');
 
         childProcess.stdout.on('data', (data) => {
-            let mailBackup = new MyNotification('Backup CRON', data.toString());
+            let mailBackup = new MyNotification('Backup CRON', data.toString().replace(/[\r\n]/, '\r\n'));
             this.mail.send(mailBackup);
         });
     }
