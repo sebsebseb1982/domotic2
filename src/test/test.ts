e58d178ae56106f7c8622930c4cf9512a3d26bc1
Seb <seb@seb.fr>
2018-05-02 17:46:07 +0200

    feat(Google Home): Premiers tests

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,4 +2,6 @@ import {GoogleHomeService} from "../notifications/services/googleHomeService";
 
 let googleHome = new GoogleHomeService();
 
-googleHome.speak('coucou Sébastien, comment ça va ?')
+googleHome.speak('coucou Sébastien, comment ça va ?');
+
+googleHome.speak('Moi ça va pas mal en tout cas.');
