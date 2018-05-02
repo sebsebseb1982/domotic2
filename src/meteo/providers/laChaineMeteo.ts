be162ad519f26a91e5febb39cc7035e1adeb3049
Seb <seb@seb.fr>
2018-05-02 20:57:31 +0200

    feat(Meteo): Ajout d'un crawler la chaine Météo

--- src/meteo/providers/laChaineMeteo.ts
+++ src/meteo/providers/laChaineMeteo.ts
@@ -1,6 +1,14 @@
+import * as cheerio from 'cheerio';
+import {IMeteo} from "../model/meteo";
 
 export class LaChaineMeteo {
     constructor(private urlPageVille: string) {
+        cheerio.load();
+    }
 
+    get meteoDuJour():IMeteo {
+        return {
+            texte:'tutu'
+        }
     }
 }
