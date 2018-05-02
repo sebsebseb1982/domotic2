976877349c1266980f366beb22d66f104aef8f7c
Seb <seb@seb.fr>
2018-05-02 22:55:08 +0200

    feat(Google Home): Test lecture MP3

--- src/test/testmp3.ts
+++ src/test/testmp3.ts
@@ -5,6 +5,6 @@ import {GenericMeteoScraper} from "../meteo/scrapers/generic";
 
 let googleHome = new GoogleHomeService();
 
-googleHome.playMedia('~/JRMelodies/51\\ Hana\\ to\\ Sora.mp3');
+googleHome.playMedia('http://192.168.1.169:5000/fsdownload/webapi/file_download.cgi/01%20Seseragi.mp3?dlink=%222f6d757369632f4a524d656c6f646965732f30312053657365726167692e6d7033%22&noCache=1525294458816&_sharing_id=%22wAI0THCMH%22&api=SYNO.FolderSharing.Download&version=2&method=download&mode=download&stdhtml=false');
 
 
