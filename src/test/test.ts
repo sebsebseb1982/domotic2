6b321d5fedc1b7ce1312a09b16c4e5d14ce3f45e
Seb <seb@seb.fr>
2018-05-02 17:25:30 +0200

    feat(Google Home): Premiers tests

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,4 +2,4 @@ import {GoogleHomeService} from "../notifications/services/googleHomeService";
 
 let googleHome = new GoogleHomeService();
 
-googleHome.playMedia('http://www.orangefreesounds.com/wp-content/uploads/2015/04/Birds-chirping-sound-morning-bird-sounds.mp3');
+googleHome.speak('coucou Sébastien, comment ça va ?')
