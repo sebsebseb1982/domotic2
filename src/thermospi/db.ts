e19cec703fccdaad28c93f37652b90c759dfcd69
Seb <seb@seb.fr>
2018-05-10 21:35:45 +0200

    feat(ventilation): init

--- src/thermospi/db.ts
+++ src/thermospi/db.ts
@@ -15,6 +15,7 @@ export class ThermospiDB {
     }
 
     get currentInsideTemperature():Promise<number> {
+        let probes = [2,3];
         return new Promise((resolve, reject) => {
             MongoClient.connect(this.configuration.thermospi.mongoURL, (err, db) => {
                 let error = 'Erreur lors de la lecture de la température intérieure de la maison : ';
@@ -24,11 +25,42 @@ export class ThermospiDB {
                 } else {
                     db.collection('temperatures').find(
                         {
-                            "probe":{$in:[2,3]}
+                            probe:{$in: probes}
                         },
                         {
                             sort: [['date','desc']],
-                            limit: 2
+                            limit: probes.length
+                        }
+                    ).toArray((err2, results:ITemperature[]) => {
+                        if (err2) {
+                            this.notifier.notifyError(error, err2);
+                            reject(err2);
+                        } else {
+                            resolve(_.mean(_.map(results, 'temperature')));
+                        }
+                        db.close();
+                    });
+                }
+            });
+        });
+    }
+
+    get currentOutsideTemperature():Promise<number> {
+        let probes = [1];
+        return new Promise((resolve, reject) => {
+            MongoClient.connect(this.configuration.thermospi.mongoURL, (err, db) => {
+                let error = 'Erreur lors de la lecture de la température intérieure de la maison : ';
+                if (err) {
+                    this.notifier.notifyError(error, err);
+                    reject(err);
+                } else {
+                    db.collection('temperatures').find(
+                        {
+                            probe:{$in: probes}
+                        },
+                        {
+                            sort: [['date','desc']],
+                            limit: probes.length
                         }
                     ).toArray((err2, results:ITemperature[]) => {
                         if (err2) {
--- src/thermospi/ventilate-house.ts
+++ src/thermospi/ventilate-house.ts
@@ -1,9 +1,11 @@
 import {GoogleHomeService} from "../notifications/services/googleHomeService";
 import {TocToc} from "../toctoc/toctoc";
+import {ThermospiDB} from "./db";
 
 export class VentilateHouse {
     googleHome: GoogleHomeService;
     toctoc: TocToc;
+    db: ThermospiDB;
 
     constructor() {
         this.googleHome = new GoogleHomeService();
@@ -12,7 +14,13 @@ export class VentilateHouse {
 
     check(): void {
         this.toctoc.ifPresent(() => {
+            Promise.all([this.db.currentInsideTemperature, this.db.currentOutsideTemperature]).then((temperatures) => {
+                let currentInsideTemperature = temperatures[0];
+                let currentOutsideTemperature = temperatures[1];
 
+                console.log('Températures intérieures : ', currentInsideTemperature);
+                console.log('Températures extérieures : ', currentOutsideTemperature);
+            });
         });
     }
 }
