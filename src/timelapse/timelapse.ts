1cd50ca7049f2f1a6274d67edfd36e627dd29c5a
Seb <seb@seb.fr>
2018-05-11 18:47:40 +0200

    feat(timelapse): init

--- src/timelapse/timelapse.ts
+++ src/timelapse/timelapse.ts
@@ -1,24 +1,31 @@
 import {Configuration} from "../configuration/configuration";
 import * as fs from 'fs';
 import * as http from 'http';
+import * as _ from "lodash";
+import {INotifier} from "../notifications/notifier";
+import {NotifyMyAndroidNotifierService} from "../notifications/services/notifyMyAndroidService";
 
 export class Timelapse {
     configuration: Configuration;
     stillImageUrl: string;
     period: number;
     occurence: number;
+    notifier: INotifier;
 
     constructor() {
         this.configuration = new Configuration();
+        this.notifier =  new NotifyMyAndroidNotifierService('Timelapse');
         let foscamR2Etage = this.configuration.cameras[0];
         this.stillImageUrl = `http://${foscamR2Etage.hostname}:${foscamR2Etage.port}/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=${foscamR2Etage.user}&pwd=${foscamR2Etage.password}`;
         this.occurence = 100;
         this.period = 100;
+
+        console.log(`This timelapse will last ${Math.round((this.occurence * this.period) / (1000 * 60))} minutes and take ${this.occurence} photos`);
     }
 
-    takePhotos():Promise<string[]> {
+    private takePhotos():Promise<string[]> {
         return new Promise<string[]>((resolve, reject) => {
-            let photos: string[] = [];
+            let photosPathes: string[] = [];
 
             for(let photoIndex = 0; photoIndex < 100; photoIndex ++) {
                 setTimeout(
@@ -27,6 +34,7 @@ export class Timelapse {
                         let photo = fs.createWriteStream(photoPath);
                         http.get(this.stillImageUrl, (response) => {
                             response.pipe(photo);
+                            photosPathes.push(photoPath);
                         });
                     },
                     photoIndex * 100
@@ -35,7 +43,7 @@ export class Timelapse {
 
             setTimeout(
                 () => {
-                    resolve(photos);
+                    resolve(photosPathes);
                 },
                 this.occurence * this.period + 1000
             );
@@ -43,9 +51,20 @@ export class Timelapse {
         });
     }
 
+    private clean(photos: string[]) {
+        _.forEach(photos, (aPhotoPath) => {
+            fs.unlink(aPhotoPath, (err) => {
+                this.notifier.notify({
+                    title: 'Impossible de supprimer une photo',
+                    description: err.message
+                });
+            });
+        });
+    }
+
     start() {
         this.takePhotos().then((photos: string[]) => {
-           console.log(photos);
+            this.clean(photos);
         });
     }
 }
