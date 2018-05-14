65bbb0eea8e05e24d49cb0e73a86e1b49c2b8d87
Seb <seb@seb.fr>
2018-05-14 23:37:12 +0200

    feat(vault): init

--- src/test/test2.ts
+++ src/test/test2.ts
@@ -2,4 +2,13 @@ import {Vault} from "../security/cctv/vault";
 
 let vault = new Vault();
 
-vault.archiveYesterdaySnaphots();
+// vault.archiveYesterdaySnaphots();
+
+
+
+import * as fs from 'fs';
+
+fs.readdirSync('/home/pi/node').forEach((file) => {
+    let fileStats = fs.statSync(`/home/pi/node/${file}`);
+    console.log(fileStats.birthtime);
+});
