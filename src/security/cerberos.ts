8bcb04c4dc80b26530ad798952995dd95ac891d0
sebsebseb <you@example.com>
2018-08-08 13:16:51 +0200

    feat(cerberos): Init

--- src/security/cerberos.ts
+++ src/security/cerberos.ts
@@ -1,4 +1,5 @@
 import {PresenceDetector} from "./cctv/presence-detector";
+import {Snapshot} from "./cctv/snapshot";
 import * as _ from "lodash";
 
 export class Cerberos {
@@ -15,9 +16,35 @@ export class Cerberos {
         let snapshotsFromCamerasWhichCanTriggerVoice = _.filter(snapshots, (snapshot) => snapshot.camera.canTriggerVoice);
         let snapshotsFromCamerasWhichCanTriggerNotification = _.filter(snapshots, (snapshot) => snapshot.camera.canTriggerNotification);
 
+        if(snapshotsFromCamerasWhichCanTriggerLight.length > 0) {
+            console.log(`La lumière va être allumée par les caméras suivantes: ${this.getCameraNamesFromSnapshots(snapshotsFromCamerasWhichCanTriggerLight)}`);
+            this.turnLightOn();
+        }
 
-        console.log(`Can trigger light: ${snapshotsFromCamerasWhichCanTriggerLight.length}`);
-        console.log(`Can trigger voice: ${snapshotsFromCamerasWhichCanTriggerVoice.length}`);
-        console.log(`Can trigger notification: ${snapshotsFromCamerasWhichCanTriggerNotification.length}`);
+        if(snapshotsFromCamerasWhichCanTriggerVoice.length > 0) {
+            console.log(`La parole va être activée par les caméras suivantes: ${this.getCameraNamesFromSnapshots(snapshotsFromCamerasWhichCanTriggerVoice)}`);
+            this.speak();
+        }
+
+        if(snapshotsFromCamerasWhichCanTriggerNotification.length > 0) {
+            console.log(`Une notification va être envoyée suite à une détection des caméras suivantes: ${this.getCameraNamesFromSnapshots(snapshotsFromCamerasWhichCanTriggerNotification)}`);
+            this.notify(snapshotsFromCamerasWhichCanTriggerNotification);
+        }
+    }
+
+    turnLightOn() {
+
+    }
+
+    speak() {
+
+    }
+
+    notify(snapshots: Snapshot[]) {
+
+    }
+
+    private getCameraNamesFromSnapshots(snapshots: Snapshot[]): String[] {
+        return _.uniq(_.map(snapshots, 'camera.label'));
     }
 }
