bb00bdb71b8f2aea6f3596938ae6bbf8bca04990
seb <titi@toto.fr>
2018-08-08 14:11:58 +0200

    feat(cerberos): Ajout du mail

--- src/notifications/services/mailService.ts
+++ src/notifications/services/mailService.ts
@@ -2,6 +2,8 @@ import {Configuration} from "../../configuration/configuration";
 import {MyNotification} from "../myNotification";
 import * as SendmailTransport from "nodemailer/lib/sendmail-transport";
 import * as Mail from "nodemailer/lib/mailer";
+import {Attachment} from "../../../node_modules/@types/nodemailer/lib/mailer";
+import * as _ from "lodash";
 
 let nodemailer = require('nodemailer');
 
@@ -17,14 +19,24 @@ export class MailService {
     }
 
     send(notification: MyNotification, mailAddressesToNotify?: string[]) {
-        let mailOptions:SendmailTransport.Options = {
+        let mailOptions: SendmailTransport.Options = {
             from: 'Maison <noreply@maison.fr>',
             to: mailAddressesToNotify ? mailAddressesToNotify : this.configuration.adminMailAddress,
             subject: `[${this.service}] ${notification.title}`,
             html: `<span>${notification.description}</span>`,
-            sendmail: true
+            sendmail: true,
+            attachments: this.getAttachmentsFromFilesPath(notification.attachments)
         };
 
         this.mailTransport.sendMail(mailOptions);
+    }
+
+    private getAttachmentsFromFilesPath(filePaths: string[]): Attachment[] {
+        return _.map(filePaths, (aFilePath) => {
+            return {
+                filename: aFilePath,
+                path: aFilePath
+            };
+        });
     }
 }
