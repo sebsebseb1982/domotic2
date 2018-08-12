1c4e2f2e6098f1334a0b5b2eafaa3e5a54140ca7
Seb <titi@toto.fr>
2018-08-12 21:03:53 +0200

    feat(PowerOutlet): init

--- src/test/test.ts
+++ src/test/test.ts
@@ -3,5 +3,7 @@ import {DB} from "../power-outlet/db";
 let db = new DB();
 
 db.getByCode('A1').then((outlet) => {
-   outlet.impulse(5 * 1000);
+    setTimeout(() => {
+        outlet.impulse(5 * 1000);
+    }, 10 * 1000);
 });
