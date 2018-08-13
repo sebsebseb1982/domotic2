229cc6ee82e0ceb5aa48a981cfd15e7040851eaf
Seb <titi@toto.fr>
2018-08-13 08:58:17 +0200

    feat(PowerOutlet): init

--- src/api/domotic/routes/outlets-routes.ts
+++ src/api/domotic/routes/outlets-routes.ts
@@ -2,10 +2,21 @@ import {Request, Response} from "express";
 import * as core from "express-serve-static-core";
 import {RFXcom} from "../../../rfxcom/RFXcom";
 import {IRoutable} from "./routes";
+import {DB} from "../../../power-outlet/db";
+import {Logger} from "../../../common/logger/logger";
 
 let rfxcom = require('rfxcom');
 
 export class OutletsRoutes implements IRoutable {
+
+    db: DB;
+    logger: Logger;
+
+    constructor() {
+        this.db = new DB();
+        this.logger = new Logger('Endpoint prises connectées');
+    }
+
     public routes(router: core.Router): void {
         router
             .post(
@@ -13,6 +24,12 @@ export class OutletsRoutes implements IRoutable {
                 (req: Request, res: Response) => {
                     let state = Boolean(req.body.state);
                     let id = req.params.id;
+
+                    this.logger.debug(`/outlet/${id} state=${state}`);
+
+                    this.db.getByCode(id).then((powerOutlet) => {
+                       powerOutlet.setState(state);
+                    });
                 }
             );
     }
