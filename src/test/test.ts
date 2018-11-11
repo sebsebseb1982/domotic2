9a5c63439ce944b452c8a98560cca23ee59afad9
Seb <titi@toto.fr>
2018-11-11 07:39:34 +0100

    feat(thermostat): ajoute un script de démarrage

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,7 @@
 import {SetPointDB} from "../thermospi/db/SetPointDB";
 import {Thermostat} from "../thermospi/thermostat";
 
-new SetPointDB().increment(-6).then(() => {
+new SetPointDB().increment(3).then(() => {
     new Thermostat().update();
 });
 
