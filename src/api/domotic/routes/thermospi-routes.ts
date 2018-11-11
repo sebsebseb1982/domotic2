ddf4eb5e3b60e99f5d4d11d41bbc639ca4ec7c13
Seb <titi@toto.fr>
2018-11-11 09:06:20 +0100

    feat(thermostat): annonce la consigne du chauffage

--- src/api/domotic/routes/thermospi-routes.ts
+++ src/api/domotic/routes/thermospi-routes.ts
@@ -3,15 +3,19 @@ import * as core from "express-serve-static-core";
 import {IRoutable} from "./routes";
 import {SetPointDB} from "../../../thermospi/db/SetPointDB";
 import {Thermostat} from "../../../thermospi/thermostat";
+import {GoogleHomeService} from "../../../notifications/services/googleHomeService";
+import {ISetPoint} from "../../../thermospi/models/setpoint";
 
 export class ThermospiRoutes implements IRoutable {
 
     setPointDB: SetPointDB;
     thermostat: Thermostat;
+    googleHomeService: GoogleHomeService;
 
     constructor() {
         this.setPointDB = new SetPointDB();
         this.thermostat = new Thermostat();
+        this.googleHomeService = new GoogleHomeService();
     }
 
     public routes(router: core.Router): void {
@@ -20,7 +24,9 @@ export class ThermospiRoutes implements IRoutable {
                 '/thermostat/setpoint',
                 (req: Request, res: Response) => {
                     let temperatureDelta = parseFloat(req.body.value);
-                    this.setPointDB.increment(temperatureDelta);
+                    this.setPointDB.increment(temperatureDelta).then((newSetPoint: ISetPoint) => {
+                        this.googleHomeService.say(`La consigne du chauffage est à ${newSetPoint.value}°C`);
+                    });
                     this.thermostat.update();
                 }
             );
