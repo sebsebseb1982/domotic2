59579d28a1d4eff35d6ac7b32ab5de412004c505
Seb <titi@toto.fr>
2018-11-18 09:34:52 +0100

    feat(Thermostat): ajoute une route d'info

--- src/api/domotic/routes/thermospi-routes.ts
+++ src/api/domotic/routes/thermospi-routes.ts
@@ -5,15 +5,19 @@ import {SetPointDB} from "../../../thermospi/db/SetPointDB";
 import {Thermostat} from "../../../thermospi/thermostat";
 import {GoogleHomeService} from "../../../notifications/services/googleHomeService";
 import {ISetPoint} from "../../../thermospi/models/setpoint";
+import {TemperatureDB} from "../../../thermospi/db/TemperatureDB";
+import * as _ from "lodash";
 
 export class ThermospiRoutes implements IRoutable {
 
     setPointDB: SetPointDB;
+    temperatureDB: TemperatureDB;
     thermostat: Thermostat;
     googleHomeService: GoogleHomeService;
 
     constructor() {
         this.setPointDB = new SetPointDB();
+        this.temperatureDB = new TemperatureDB();
         this.thermostat = new Thermostat();
         this.googleHomeService = new GoogleHomeService();
     }
@@ -28,6 +32,28 @@ export class ThermospiRoutes implements IRoutable {
                         this.googleHomeService.say(`La consigne du chauffage est à ${newSetPoint.value}°C`);
                     });
                     this.thermostat.update();
+                }
+            )
+            .get(
+                '/thermostat/temperatures',
+                (req: Request, res: Response) => {
+                    Promise.all([
+                        this.temperatureDB.getCurrentInsideTemperature(),
+                        this.temperatureDB.getCurrentOutsideTemperature(),
+                        this.setPointDB.getCurrentSetPoint()
+                    ]).then((temperatures) => {
+                        let currentInsideTemperature = temperatures[0];
+                        let currentOutsideTemperature = temperatures[1];
+                        let currentSetPoint = temperatures[2];
+
+                        this.googleHomeService.say(`Actuellement, il fait ${_.round(currentInsideTemperature, 1)}°C à l'intérieur de la maison, ${_.round(currentOutsideTemperature, 1)}°C à l'extérieur et la consigne du chauffage est réglée à ${_.round(currentSetPoint.value, 1)}°C.`);
+
+                        res.send({
+                            inside: currentInsideTemperature,
+                            outside: currentOutsideTemperature,
+                            setPoint: currentSetPoint
+                        });
+                    });
                 }
             );
     }
