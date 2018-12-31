8bc940ea1e4b637b29a67e570ec7cfb217b19a5e
Seb <titi@toto.fr>
2018-12-31 19:22:01 +0100

    test(auth): test

---
+++ src/notifications/services/pushover-service.ts
@@ -0,0 +1,47 @@
+import * as https from "https";
+import {MyNotification} from "../myNotification";
+import {Configuration} from "../../configuration/configuration";
+
+export class PushoverService {
+    configuration : Configuration;
+    constructor () {
+        this.configuration = new Configuration();
+    }
+
+    send(notification: MyNotification) {
+        let postData = JSON.stringify({
+            token: this.configuration.pushover.token,
+            user: this.configuration.pushover.user,
+            title: notification.title,
+            message: notification.description,
+            priority: notification.priority ? notification.priority : 0
+        });
+
+        let options = {
+            hostname: 'api.pushover.net',
+            port: 443,
+            path: '/1/messages.json',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': postData.length
+            }
+        };
+
+        let req = https.request(options, (res) => {
+            console.log('statusCode:', res.statusCode);
+            console.log('headers:', res.headers);
+
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+        });
+
+        req.on('error', (e) => {
+            console.error(e);
+        });
+
+        req.write(postData);
+        req.end();
+    }
+}
