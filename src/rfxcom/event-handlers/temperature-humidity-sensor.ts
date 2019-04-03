9e6fbb9cd76da14d27400cf023fa2726db41b556
Seb <titi@toto.fr>
2019-04-03 16:48:33 +0200

    feat(thermospi): Ajout d'un capteur d'humidité

---
+++ src/rfxcom/event-handlers/temperature-humidity-sensor.ts
@@ -0,0 +1,24 @@
+import {Logger} from "../../common/logger/logger";
+import {TemperatureDB} from "../../thermospi/db/TemperatureDB";
+
+export class TemperatureHumiditySensor{
+    logger: Logger;
+    temperatureDB: TemperatureDB;
+    constructor(private rfxcom: any){
+        this.temperatureDB = new TemperatureDB();
+        this.logger = new Logger('Sonde de température/humidité 433 MHz');
+    }
+
+    listen() {
+        this.rfxcom.on("temperaturehumidity1", (event) => {
+            console.log(event);
+            this.logger.debug(`Sonde de température/humidité ${event.id} (${event.temperature}°C, batterie = ${event.batteryLevel})`);
+            this.temperatureDB.saveTemperatures([{
+                value: event.temperature,
+                probe: event.id,
+                batteryLevel: event.batteryLevel,
+                date: new Date()
+            }]);
+        });
+    }
+}
