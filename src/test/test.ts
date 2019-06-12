ff2bb5e476a0a053b61658b31086779cc97c712a
Seb <titi@toto.fr>
2019-06-12 19:39:32 +0200

    feat(notification): notifie au reboot

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,10 @@
 import {PushoverService} from "../notifications/services/pushover-service";
+import {GoogleHomeService} from "../notifications/services/googleHomeService";
 
 new PushoverService().send({
     title: 'test',
     description:'description',
     priority:0
 });
+
+new GoogleHomeService().say('coucou');
