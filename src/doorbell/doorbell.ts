e06dacc2e82f227124dba60c8f2168dc1ea3afe0
Seb <titi@toto.fr>
2019-07-13 20:25:13 +0200

    feat(sonnette): test

--- src/doorbell/doorbell.ts
+++ src/doorbell/doorbell.ts
@@ -6,5 +6,6 @@ export class Doorbell {
         gpio.on('change', (channel, value) => {
             console.log('Channel ' + channel + ' value is now ' + value);
         });
+        gpio.setup(3, gpio.DIR_IN, gpio.EDGE_BOTH);
     }
 }
