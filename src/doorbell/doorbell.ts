fa0b173fb6985f53e9d3a685333c87d780e17d1e
Seb <titi@toto.fr>
2019-07-13 20:39:40 +0200

    feat(sonnette): test

--- src/doorbell/doorbell.ts
+++ src/doorbell/doorbell.ts
@@ -3,9 +3,24 @@ import * as gpio from 'rpi-gpio';
 
 export class Doorbell {
     constructor() {
-        gpio.on('change', (channel, value) => {
+        /*gpio.on('change', (channel, value) => {
             console.log('Channel ' + channel + ' value is now ' + value);
         });
         gpio.setup(3, gpio.DIR_IN, gpio.EDGE_BOTH);
+        */
+
+        while (true) {
+            gpio.setup(3, gpio.DIR_IN, readInput);
+
+            function readInput(err) {
+                if (err) throw err;
+                gpio.read(3, function (err, value) {
+                    if (err) throw err;
+                    console.log('The value is ' + value);
+                });
+            }
+        }
+
+
     }
 }
