ac7e2261a9015687c91a702a59e886c07c5d3877
Seb <titi@toto.fr>
2019-07-13 20:56:53 +0200

    feat(sonnette): test

--- src/doorbell/doorbell.ts
+++ src/doorbell/doorbell.ts
@@ -1,26 +1,27 @@
-import * as gpio from 'rpi-gpio';
+import * as gpio from 'gpio';
 
 
 export class Doorbell {
     constructor() {
-        /*gpio.on('change', (channel, value) => {
-            console.log('Channel ' + channel + ' value is now ' + value);
-        });
-        gpio.setup(3, gpio.DIR_IN, gpio.EDGE_BOTH);
-        */
+        var gpio4 = gpio.export(3, {
+            // When you export a pin, the default direction is out. This allows you to set
+            // the pin value to either LOW or HIGH (3.3V) from your program.
+            direction: gpio.DIRECTION.IN,
 
-        while (true) {
-            gpio.setup(3, gpio.DIR_IN, readInput);
+            // set the time interval (ms) between each read when watching for value changes
+            // note: this is default to 100, setting value too low will cause high CPU usage
+            interval: 200,
 
-            function readInput(err) {
-                if (err) throw err;
-                gpio.read(3, function (err, value) {
-                    if (err) throw err;
-                    console.log('The value is ' + value);
+            // Due to the asynchronous nature of exporting a header, you may not be able to
+            // read or write to the header right away. Place your logic in this ready
+            // function to guarantee everything will get fired properly
+            ready: function() {
+                gpio4.on("change", function(val) {
+                    // value will report either 1 or 0 (number) when the value changes
+                    console.log(val)
                 });
             }
-        }
-
+        });
 
     }
 }
